d2e29a9e7524726aacefcab4f607d8c765cc818c
PabloLucas2005 <pablolucasfarias@gmail.com>
2023-03-28 20:00:19 -0300

    teste

---
+++ Estrutura de Dados/Vetor/src/App.java
@@ -0,0 +1,20 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int[] vetorzinho = new int[10];
+
+      vetorzinho[0] = 1;
+      vetorzinho[3] = 43;
+      vetorzinho[2] = 35;
+      vetorzinho[5] = 90;
+      vetorzinho[1] = 83;
+      vetorzinho[4] = 20;
+      vetorzinho[6] = 47;
+      vetorzinho[8] = 16;
+      vetorzinho[7] = 85;
+      vetorzinho[9] = 3;
+
+      for(int i = 0; i <= vetorzinho.length; i++){
+        System.out.println(vetorzinho[i]);
+      }
+    }
+}
